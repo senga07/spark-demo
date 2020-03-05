ca0017a6d66f091431160c884bb94642838277e0
黄思佳 <huangsijia@easybaocn.com>
2020-03-05 23:01:06 +0800

    Window operation

--- src/main/scala/com/hlsijx/spark/stream/wordcount/NetworkWordCount.scala
+++ src/main/scala/com/hlsijx/spark/stream/wordcount/NetworkWordCount.scala
@@ -1,7 +1,8 @@
 package com.hlsijx.spark.stream.wordcount
 
-import com.hlsijx.spark.{CommonConfig}
+import com.hlsijx.spark.CommonConfig
 import com.hlsijx.spark.stream.factory.SparkStreamFactory
+import org.apache.spark.streaming.Seconds
 
 /**
   * Counts words in UTF8 encoded, '\n' delimited text received from the network every second.
@@ -19,10 +20,14 @@ object NetworkWordCount {
 
     val lines = ssc.socketTextStream(CommonConfig.hostname, CommonConfig.port)
 
-    val wordCount = lines.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+    val wordCount = lines.flatMap(_.split(" ")).map((_, 1))//.reduceByKey(_ + _)
+      //Reduce last 30 seconds of data, every 10 seconds 每过10s统计一次最后30s内的数据
+      .reduceByKeyAndWindow((a:Int,b:Int) => a + b, Seconds(30), Seconds(10))
 
     wordCount.print()
 
     SparkStreamFactory.startStreamingJob(ssc)
   }
+
+
 }
